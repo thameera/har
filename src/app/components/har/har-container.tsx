2759492182400579eea880275c436e50ac16d4d1
Thameera Senanayaka <me@thameera.com>
2025-03-12 23:07:54 +1100

    Move main view to har-container.tsx

---
+++ src/app/components/har/har-container.tsx
@@ -0,0 +1,3 @@
+export default function HarContainer() {
+  return <h1 className="text-xl font-bold">HAR Analyzer</h1>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import HarContainer from "./components/har/har-container";
+
 export default function Home() {
+  /*
+   * We keep the main component in /components/har to make it compatible with
+   * the Toolbox project layout.
+   */
   return (
     <div className="min-h-screen p-8">
-      <h1 className="text-xl font-bold">HAR Analyzer</h1>
+      <HarContainer />
     </div>
   );
 }
