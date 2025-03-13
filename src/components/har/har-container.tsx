d0d29fc8fb3e23039bda8bde941e3bf9c7ec0c0c
Thameera Senanayaka <me@thameera.com>
2025-03-13 22:15:48 +1100

    Add dark mode toggle

--- src/components/har/har-container.tsx
+++ src/components/har/har-container.tsx
@@ -1,9 +1,13 @@
+import { ModeToggle } from "../mode-toggle";
 import HarView from "./har-view";
 
 export default function HarContainer() {
   return (
     <div>
-      <h1 className="text-xl font-bold">HAR Analyzer</h1>
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-bold">HAR Analyzer</h1>
+        <ModeToggle />
+      </div>
       <HarView />
     </div>
   );
