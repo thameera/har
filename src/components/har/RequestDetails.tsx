370f67001a49ecf98cbaedabfe6135500a4e07c5
Thameera Senanayaka <me@thameera.com>
2025-03-26 21:14:01 +1100

    Move fancy url to its own commit

--- src/components/har/RequestDetails.tsx
+++ src/components/har/RequestDetails.tsx
@@ -1,4 +1,5 @@
 import { HarRequest } from "./harTypes";
+import { FancyURL } from "./FancyURL";
 
 interface RequestDetailsProps {
   request: HarRequest;
@@ -7,69 +8,9 @@ interface RequestDetailsProps {
 export function RequestDetails({ request }: RequestDetailsProps) {
   const { method, url } = request.request;
 
-  const renderUrl = () => {
-    try {
-      const urlObj = new URL(url);
-      const searchParams = new URLSearchParams(urlObj.search);
-      const hash = urlObj.hash.slice(1); // Remove the # symbol
-      const hashParams = new URLSearchParams(hash);
-
-      return (
-        <div className="font-mono text-sm break-all">
-          <span className="text-gray-900 dark:text-gray-100">{method}</span>
-          <span className="text-gray-600 dark:text-gray-400"> </span>
-          <span className="text-gray-600 dark:text-gray-400">
-            {urlObj.protocol}//
-          </span>
-          <span className="text-blue-500 dark:text-blue-400 font-medium">
-            {urlObj.hostname}
-          </span>
-          <span className="text-gray-600 dark:text-gray-400">
-            {urlObj.pathname}
-          </span>
-          {urlObj.search && (
-            <span className="text-gray-600 dark:text-gray-400">
-              {Array.from(searchParams.entries()).map(([key, value], index) => (
-                <span key={key}>
-                  {index === 0 ? "?" : "&"}
-                  <span className="text-purple-500 dark:text-purple-400">
-                    {key}
-                  </span>
-                  <span className="text-gray-600 dark:text-gray-400">
-                    ={value}
-                  </span>
-                </span>
-              ))}
-            </span>
-          )}
-          {hash && (
-            <span className="text-gray-600 dark:text-gray-400">
-              #
-              {Array.from(hashParams.entries()).map(([key, value], index) => (
-                <span key={key}>
-                  {index === 0 ? "" : "&"}
-                  <span className="text-purple-500 dark:text-purple-400">
-                    {key}
-                  </span>
-                  <span className="text-gray-600 dark:text-gray-400">
-                    ={value}
-                  </span>
-                </span>
-              ))}
-            </span>
-          )}
-        </div>
-      );
-    } catch {
-      return (
-        <div className="font-mono text-sm break-all">
-          <span className="text-gray-900 dark:text-gray-100">{method}</span>
-          <span className="text-gray-600 dark:text-gray-400"> </span>
-          <span className="text-gray-600 dark:text-gray-400">{url}</span>
-        </div>
-      );
-    }
-  };
-
-  return <div className="p-4">{renderUrl()}</div>;
+  return (
+    <div className="p-4 space-y-2">
+      <FancyURL method={method} url={url} />
+    </div>
+  );
 }
