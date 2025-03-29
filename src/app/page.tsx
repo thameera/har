857219eb941e5739d1d6c465d31ff99085e7a96c
Thameera Senanayaka <me@thameera.com>
2025-03-29 13:22:54 +1100

    Move title row to page.tsx
    We don't want to keep that part in har container, which could later
    be copied to toolbox.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import HarContainer from "@/components/har/har-container";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
   /*
@@ -9,7 +10,13 @@ export default function Home() {
    */
   return (
     <div className="min-h-screen p-8">
-      <HarContainer />
+      <div className="flex flex-col h-[calc(100vh-4rem)] gap-4">
+        <div className="flex items-center justify-between">
+          <h1 className="text-xl font-bold">HAR Analyzer</h1>
+          <ModeToggle />
+        </div>
+        <HarContainer />
+      </div>
     </div>
   );
 }
--- src/components/har/har-container.tsx
+++ src/components/har/har-container.tsx
@@ -1,19 +1,12 @@
-import { ModeToggle } from "../mode-toggle";
 import HarView from "./har-view";
 import { HarProvider } from "./har-provider";
 
 export default function HarContainer() {
   return (
-    <div className="flex flex-col h-[calc(100vh-4rem)] gap-4">
-      <div className="flex items-center justify-between">
-        <h1 className="text-xl font-bold">HAR Analyzer</h1>
-        <ModeToggle />
-      </div>
-      <div className="flex-1 min-h-0">
-        <HarProvider>
-          <HarView />
-        </HarProvider>
-      </div>
+    <div className="flex-1 min-h-0">
+      <HarProvider>
+        <HarView />
+      </HarProvider>
     </div>
   );
 }
