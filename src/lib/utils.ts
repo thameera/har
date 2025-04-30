125ed9d2a9e5c1e4c4685b839077031b7ca5ee5f
Emmanuel Paz <emmanuel.paz@okta.com>
2025-04-29 18:20:30 -0600

    removing unsused package

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,5 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { XMLParser } from "fast-xml-parser";
 import { HarRequest } from "@/components/har/harTypes";
 
 export function cn(...inputs: ClassValue[]) {
